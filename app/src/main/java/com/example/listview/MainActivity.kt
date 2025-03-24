c8d20e837ef433398f9103f3eff674fd3fa2a803
DEV <pattnaikd833@gmail.com>
2025-03-24 18:28:17 +0530

    feat: Populate ListView with programming languages
    - Updated `misc.xml` to set the project JDK to `jbr-21`.
    - Modified `MainActivity.kt` to populate a ListView with a list of programming languages.
    - Added an `ArrayAdapter` to handle the list data and updated `listView` to use the adapter.

--- app/src/main/java/com/example/listview/MainActivity.kt
+++ app/src/main/java/com/example/listview/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.listview
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val users = arrayOf(
+            "Python", "Java", "Kotlin", "C++", "JavaScript", "Ruby", "Swift", "Go", "Rust", "TypeScript",
+            "PHP", "C#", "Dart", "Perl", "Scala", "R", "Haskell", "Lua", "Objective-C", "F#",
+            "Elixir", "Clojure", "Groovy", "Shell", "SQL", "MATLAB", "Julia", "Erlang", "VB.NET", "COBOL"
+        )
+        val listView: ListView = findViewById(R.id.listView)
+        val listAdapter: ArrayAdapter<String> = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            users
+        )
+
+        listView.adapter = listAdapter
     }
 }
